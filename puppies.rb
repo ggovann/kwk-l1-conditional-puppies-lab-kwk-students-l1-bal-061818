958ae4ce40c4bdfd619fd35604df805e0ce4a93e
Grace Govan <ggovan11@outlook.com>
2018-06-20 15:10:54 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -24,7 +24,7 @@ def some_puppies(number_of_puppies_found, max)
     puts "get back to puppy restriction"
   end
 end
-some_puppies(3)
+some_puppies(6,4)
 
 # 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
 def new_animal_accumulation(num_puppies, numb_cats)
